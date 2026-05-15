c9407d685b74f0e01c4f519cf0b83e59acc922f5
JakubTvrdy <150028642+JakubTvrdy@users.noreply.github.com>
2026-05-15 21:22:14 +0200

    Update script.js

--- script.js
+++ script.js
@@ -104,21 +104,3 @@ if (formular) {
         });
     });
 }
-
-
-// =========================================
-// 4. EFEKT ODHALENÍ PŘI SCROLLOVÁNÍ
-// =========================================
-function revealElements() {
-    let reveals = document.querySelectorAll('.reveal');
-    for (let i = 0; i < reveals.length; i++) {
-        let windowHeight = window.innerHeight;
-        let elementTop = reveals[i].getBoundingClientRect().top;
-        let elementVisible = 100;
-        if (elementTop < windowHeight - elementVisible) {
-            reveals[i].classList.add('active');
-        }
-    }
-}
-window.addEventListener('scroll', revealElements);
-revealElements();
